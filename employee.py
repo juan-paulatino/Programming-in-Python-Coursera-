db5cfe81c080bb34d8bd14a2d7d8ea6940c028e4
Juan Carlos Paulino Fonseca <118320209+juan-paulatino@users.noreply.github.com>
2023-08-08 01:41:35 -0700

    employee file

---
+++ employee.py
@@ -0,0 +1,8 @@
+employee_name = "Jose"
+age = "45"
+title = "owner"
+
+def details():
+    print("Employee name is:  ", employee_name)
+    print("Employee age is: ", age)
+    print("Employee title is:  ", title)

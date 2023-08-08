2d6bb82351faadbe1a27bc58536c1023c5d6184e
Juan Carlos Paulino Fonseca <118320209+juan-paulatino@users.noreply.github.com>
2023-08-08 15:09:32 -0700

    nombre, edad, puesto

---
+++ employee.py
@@ -0,0 +1,8 @@
+employee_name = "Lidia"
+age = "25"
+title = "la_patrona"
+
+def details():
+    print("Employee name is:  ", employee_name)
+    print("Employee age is: ", age)
+    print("Employee title is:  ", title)
